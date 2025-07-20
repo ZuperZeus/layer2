73648d1068461440de350b76c0a6873af0a0ede4
ZuperZeus <jorge.ochoa.arteaga@gmail.com>
2025-07-20 13:22:31 -0700

    cleaned up css

--- js/header_footer.js
+++ js/header_footer.js
@@ -5,7 +5,7 @@ function updateHeader()
     <ul class="header-left">
         <li class="header-logo">
           <a href="index.html">
-            <img src="./assets/logos/icon_logo.svg">
+            <img src="./assets/img/icon_logo.svg">
           </a>
         </li>
         <li>
