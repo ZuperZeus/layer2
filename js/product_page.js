d47c1aafd4922621eb0b2ce7cd75d46757e2f24b
ZuperZeus <jorge.ochoa.arteaga@gmail.com>
2025-07-20 21:08:32 -0700

    added solid colors

--- js/product_page.js
+++ js/product_page.js
@@ -13,10 +13,34 @@ function generateProductPage()
                 const productSection = document.querySelector('#product');
                 productSection.innerHTML = `
                 <div class="title large-title" id="product-title">${product.title}</div>
-                <img class="round-img" src="./assets/products/${product.id}/1.jpg">
-                <div class="text"><a href="#">${product.description}</a></div>
-                <div class="shop-card-price">Contact for pricing</div>
+                <div class="side-by-side">
+                    <div class="flex-content">
+                        <img class="round-img" src="./assets/products/${product.id}/1.jpg">
+                    </div>
+                    <div class="flex-content shop-card">
+                        <div class="text large-text"><a href="#">${product.description}</a></div>
+                        <div class="color-select"></div>
+                        <div class="shop-card-price">Contact for pricing</div>
+                    </div>
+                </div>
                     `;
+                const colors = fetch('/data/colors.json')
+                    .then(response => response.json())
+                    .then(colors => {
+                        const colorsDiv = document.querySelector('.color-select');
+                        colors.forEach(color => {
+                            if(!color.tricolor)
+                            {
+                                const colorCard = document.createElement('div');
+                                colorCard.classList.add("color-card");
+                                const colorCircle = document.createElement('div');
+                                colorCircle.classList.add("color-circle");
+                                colorCircle.style=`background:${color.rgb};`;
+                                colorCard.appendChild(colorCircle);
+                                colorsDiv.appendChild(colorCard);
+                            }
+                        })
+                    })
             }
             else {
                 window.open("/404.html", "_self");
