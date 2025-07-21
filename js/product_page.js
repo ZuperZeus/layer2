1995567cf31322772659d65dac4c6f19a8a6ce77
ZuperZeus <jorge.ochoa.arteaga@gmail.com>
2025-07-21 11:57:05 -0700

    Added color display to Product Page

--- js/product_page.js
+++ js/product_page.js
@@ -5,9 +5,16 @@ function getURLQuery(key)
 }
 function addURLQuery(key,val)
 {
+    console.log(`adding url query ${key} -> ${val}`)
     const urlParams = new URLSearchParams(window.location.search);
-    urlParams.append(key,val);
+    urlParams.set(key,val);
     window.location.search=urlParams;
+}
+function getNewLink(key,val)
+{
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set(key,val);
+    return urlParams;
 }
 function getTricolorText(triRGB)
 {
@@ -18,6 +25,18 @@ function getTricolorText(triRGB)
     radial-gradient(circle at 0% 0%, ${color3}, transparent 100%);
     background-blend-mode: screen; /* or lighten */
     `;
+}
+function getColorCircle(color)
+{
+    const colorCircle = document.createElement('div');
+          colorCircle.classList.add("color-circle");
+          colorCircle.classList.add(color.id);
+          if(!color.tricolor)
+            colorCircle.style=`background:${color.rgb};`;
+          else
+            colorCircle.style=getTricolorText(color.rgb);
+    return colorCircle;
+
 }
 function generateProductPage() 
 {
@@ -33,9 +52,10 @@ function generateProductPage()
                     <div class="flex-content">
                         <img class="round-img" src="./assets/products/${product.id}/1.jpg">
                     </div>
-                    <div class="flex-content">
+                    <div class="flex-content product-description">
                         <div class="text large-text"><a href="#">${product.description}</a></div>
-                        <div class="color-select"></div>
+                        <div class="color-display"></div>
+                        <div class="color-selected img-with-description"></div>
                         <div class="shop-card-price">Contact for pricing</div>
                     </div>
                 </div>
@@ -43,27 +63,28 @@ function generateProductPage()
                 const colors = fetch('/data/colors.json')
                     .then(response => response.json())
                     .then(colors => {
-                        const colorsDiv = document.querySelector('.color-select');
+                        const colorsDiv = document.querySelector('.color-display');
                         colors.forEach(color => {
-                            const colorCard = document.createElement('div');
-                            colorCard.addEventListener(onclick, () => addURLQuery('color',`${color.id}`))
-                            colorCard.classList.add("color-card");
-                            const colorCircle = document.createElement('div');
-                            colorCircle.classList.add("color-circle");
-                            // const card = document.createElement('a');
-                            if(!color.tricolor)
-                            {
-                                colorCircle.style=`background:${color.rgb};`;
-                                colorCard.appendChild(colorCircle);
-                                colorsDiv.appendChild(colorCard);
-                            }
-                            else
-                            {
-                                colorCircle.style=getTricolorText(color.rgb);
-                                colorCard.appendChild(colorCircle);
-                                colorsDiv.appendChild(colorCard);
-                            }
+                            const colorCard = document.createElement('a');
+                                //   colorCard.addEventListener(onclick, () => addURLQuery('color',color.id));
+                                  colorCard.href = "/product.html?"+getNewLink('color',color.id);
+                                  colorCard.classList.add("color-card");
+                                  colorCard.classList.add(color.id);
+                                  colorCard.appendChild(getColorCircle(color));
+                            colorsDiv.appendChild(colorCard);
                         })
+                        const colorSelect = document.querySelector(".color-selected");
+                            colorSelect.style.padding = "20px";
+                        const colorTxt = document.createElement("div");
+                            colorTxt.classList.add("text");
+                            colorTxt.classList.add("medium-text");
+                              selectedColor = colors.find(c => c.id === getURLQuery('color'));
+                              if(selectedColor)
+                              {
+                                  colorSelect.appendChild(getColorCircle(selectedColor));
+                                  colorTxt.innerText = "Selected Color: "+selectedColor.name;
+                              }
+                        colorSelect.appendChild(colorTxt);
                     })
             }
             else {
