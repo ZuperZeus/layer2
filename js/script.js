6f1a6effb8f963070c3b5362c44c0b180c967c6a
ZuperZeus <jorge.ochoa.arteaga@gmail.com>
2025-07-23 00:42:32 -0600

    Changed script.js to jquery and added zoom on scroll effect to index.html

--- js/script.js
+++ js/script.js
@@ -2,14 +2,27 @@ import * as update_header from './update_header.js';
 import * as header_footer from './header_footer.js';
 import * as shop from './shop.js';
 import * as product_page from './product_page.js';
-window.addEventListener('DOMContentLoaded', () =>
+import * as start_page from './start_page.js';
+$(window).on('DOMContentLoaded',function()
+// window.addEventListener('DOMContentLoaded', () =>
     {
         header_footer.updateHeader();
         header_footer.updateFooter();
         shop.updateProducts();
         product_page.generateProductPage();
     });
-window.addEventListener('load', () => update_header.adjustWelcomeHeight());
-window.addEventListener('resize', () => update_header.adjustWelcomeHeight());
-window.addEventListener('load', () => update_header.updateHeaderTextColor());
-window.addEventListener('scroll', () => update_header.updateHeaderTextColor());
+$(window).on('load',function()
+    {
+        update_header.adjustWelcomeHeight();
+        update_header.updateHeaderTextColor();
+    });
+$(window).on('resize',function()
+    {
+        update_header.adjustWelcomeHeight();
+        update_header.updateHeaderTextColor();
+    });
+$(window).on('scroll',function(){
+    $('.welcome-section div').css('font-size','calc(20vmin + '+(($(this).scrollTop()*0.3))+'px');
+    // $('.welcome-section .layer2').css('font-size',(($(this).scrollTop()*0.5)+14)+'px')
+    // $('.welcome-section .studios').css('font-size',(($(this).scrollTop()*0.5)+14)+'px')
+});
